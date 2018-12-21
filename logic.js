d2fcd1646782c8712e3b60bf1bc64b16ec94e002
Marcus Hilaire <marcushilaire@gmail.com>
2018-12-21 11:03:50 -0400

    Further cleaning and testing

--- logic.js
+++ logic.js
@@ -1,14 +1,5 @@
 const fs = require("fs");
 const helpers = require("./helpers/helpers.js");
-let input = {
-  strikes: "x-x-x-x-x-x-x-x-x-x-xx", //300
-  spares: "5/-5/-5/-5/-5/-5/-5/-5/-5/-5/-5", //150
-  openGame: "09-18-27-36-45-54-63-72-81-90", //90
-  endSpare: "09-18-27-36-45-54-63-72-81-7/-5", //96
-  midSpare: "24-35-24-32-45-5/-34-23-45-23", //73
-  multiSpare: "7/-5/-6/-36-81-63-8/-53-27-9/-5", //118
-  randomStrikes: "x-x-9/-43-53-72-x-72-72-5/-7" //141
-};
 
 const callback = (err, data) => {
   if (err) throw err;
@@ -24,3 +15,4 @@ const tallyScore = inputArr => {
   let frameByThrow = inputArr.map(helpers.initializeFrame); // splits frames into two throws
   return frameByThrow.reduce(helpers.reducer, 0);
 }; //returns total score at the end of game
+module.exports = { tallyScore };
--- testing/helpers.test.js
+++
@@ -1,4 +0,0 @@
-const helpers = require("../helpers/helpers.js");
-test("Is the frame being initialized correctly?", () => {
-  expect(helpers.initializeFrame("xx").toEqual(["x", "x"]));
-});
