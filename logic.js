7581e3d7fd997ffc92c9248986a286c40669aab7
Marcus Hilaire <marcushilaire@gmail.com>
2018-12-21 13:54:27 -0400

    Refactoring and clarification

--- logic.js
+++ logic.js
@@ -2,32 +2,32 @@ const fs = require("fs");
 const helpers = require("./helpers/helpers.js");
 let userOptions = process.argv.splice(2);
 
-const handleTextInput = (err, data) => {
-  if (err) throw err;
-  let inputFromFile = data.replace(/\r/g, "").split("\n");
-  inputFromFile.forEach(element => {
-    console.log(tallyScore(element));
-  });
-};
-
 const tallyScore = inputString => {
   let frameArray = inputString.split("-");
   let finalScore = frameArray.reduce(helpers.reducer, 0);
   return `The final score for the frames: ${inputString} is ${finalScore}`;
 };
 
-const main = optionsArr => {
-  switch (optionsArr[0]) {
-    case "single":
-      console.log(tallyScore(optionsArr[1]));
-      break;
-    case "multiple":
-    default:
-      console.log("Reading scores from input.txt \n");
-      fs.readFile("./data/input.txt", "utf8", handleTextInput);
-      break;
+const logFinalScore = x => {
+  console.log(`${tallyScore(x)} \n`);
+};
+
+const handleTextInput = (err, data) => {
+  if (err) throw err;
+  let inputFromFile = data.replace(/\r/g, "").split("\n");
+  inputFromFile.forEach(logFinalScore);
+};
+
+const handleUserInput = optionsArr => {
+  if (optionsArr.length > 0) {
+    // Calculate final score of all games passed to command line
+    optionsArr.forEach(logFinalScore);
+  } else {
+    // Calculate scores in input.txt if non are passed through command line
+    console.log("Reading scores from input.txt \n");
+    fs.readFile("./data/input.txt", "utf8", handleTextInput);
   }
 };
 
-main(userOptions);
+handleUserInput(userOptions);
 module.exports = { tallyScore };
